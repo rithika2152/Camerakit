0b6087da1d3b4031d79715091fec71c4621b349a
Rithika <glrithika11396@gmail.com>
2025-03-20 17:31:58 +0530

    update1
    update1

---
+++ script.ts
@@ -0,0 +1,90 @@
+import { bootstrapCameraKit, createMediaStreamSource } from '@snap/camera-kit';
+
+const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
+const videoContainer = document.getElementById(
+  'video-container'
+) as HTMLElement;
+const videoTarget = document.getElementById('video') as HTMLVideoElement;
+const startRecordingButton = document.getElementById(
+  'start'
+) as HTMLButtonElement;
+const stopRecordingButton = document.getElementById(
+  'stop'
+) as HTMLButtonElement;
+const downloadButton = document.getElementById('download') as HTMLButtonElement;
+
+let mediaRecorder: MediaRecorder;
+let downloadUrl: string;
+
+async function init() {
+  const cameraKit = await bootstrapCameraKit({
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM0NjkzMzE0LCJzdWIiOiIzMmZhMmZlNC0wYmYxLTQ2N2QtODM4Mi04MTVmNzliMjFkMWJ-U1RBR0lOR35iMWI3YmFjZi01ZGI3LTQxMDAtOWIzNC0zZDEwODJmNmMyYTAifQ.681nZeSjvWHzWBiZbHY-T3Yq6M6qk3j9zgk9nM6I06M',
+  });
+
+  const session = await cameraKit.createSession({ liveRenderTarget });
+
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+
+  const source = createMediaStreamSource(mediaStream);
+
+  await session.setSource(source);
+  await session.play();
+
+  const { lenses } = await cameraKit.lensRepository.loadLensGroups([
+    'fb6410ff-f10b-438a-a8b0-70cc6012b2b6","11cbcba2-1275-47ec-9916-feaa6c52d24b'
+  ]);
+
+  session.applyLens(lenses[0]);
+
+  bindRecorder();
+}
+
+function bindRecorder() {
+  startRecordingButton.addEventListener('click', () => {
+    startRecordingButton.disabled = true;
+    stopRecordingButton.disabled = false;
+    downloadButton.disabled = true;
+    videoContainer.style.display = 'none';
+
+    const mediaStream = liveRenderTarget.captureStream(30);
+
+    mediaRecorder = new MediaRecorder(mediaStream);
+    mediaRecorder.addEventListener('dataavailable', (event) => {
+      if (!event.data.size) {
+        console.warn('No recorded data available');
+        return;
+      }
+
+      const blob = new Blob([event.data]);
+
+      downloadUrl = window.URL.createObjectURL(blob);
+      downloadButton.disabled = false;
+
+      videoTarget.src = downloadUrl;
+      videoContainer.style.display = 'block';
+    });
+
+    mediaRecorder.start();
+  });
+
+  stopRecordingButton.addEventListener('click', () => {
+    startRecordingButton.disabled = false;
+    stopRecordingButton.disabled = true;
+
+    mediaRecorder?.stop();
+  });
+
+  downloadButton.addEventListener('click', () => {
+    const link = document.createElement('a');
+
+    link.setAttribute('style', 'display: none');
+    link.href = downloadUrl;
+    link.download = 'camera-kit-web-recording.webm';
+    link.click();
+    link.remove();
+  });
+}
+
+init();
